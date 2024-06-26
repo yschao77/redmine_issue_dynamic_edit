06a347f696a116720a73b1423e1cc9d202352837
yschao77 <yschao77@gmail.com>
2024-06-26 17:34:48 +0800

    modified css for description preview show up

--- init.rb
+++ init.rb
@@ -7,6 +7,6 @@ Redmine::Plugin.register :redmine_issue_dynamic_edit do
   author 'Hugo Zilliox'
   description 'Allows users to dynamically update issue attributes in detailed view without refreshing the page (JIRA style)'
   version '0.9.2'
-  url 'https://github.com/ilogeek/redmine_issue_dynamic_edit'
+  url 'https://github.com/yschao77/redmine_issue_dynamic_edit'
   author_url 'https://hzilliox.fr'
 end
